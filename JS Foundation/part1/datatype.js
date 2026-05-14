45dc8c7e2372ba9f5d74386585e2c78395281ac4
2chandan1 <Tiwarichandan936@gmail.com>
2026-05-14 11:38:32 +0530

    Starting Js

---
+++ JS Foundation/part1/datatype.js
@@ -0,0 +1,29 @@
+/**
+String
+
+Number
+
+Boolean
+
+Bigint
+
+Undefined :- means there is no definition for it . 
+
+null :-  means empty, epmty does not mean 0, 
+
+Object
+
+Symbol
+
+**/
+
+var score=120;
+let score =123;
+let name="john";
+let isLoggedIn=true;
+let teatypes=["green tea","black tea","lemon tea"];
+let user={name:"john",age:30};
+
+let getscroe=score;
+
+console.log(typeof score);
